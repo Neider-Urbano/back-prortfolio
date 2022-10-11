2546fe357e226033b0e6b124880197a8d81b24fd
Neider Urbano <jjuly8julianur@gmail.com>
2022-10-11 13:44:24 -0500

    files backend

--- microservices/email/route.ts
+++ microservices/email/route.ts
@@ -3,5 +3,8 @@ import postMail from "./controller"
 const routerMail = Router();
 
 routerMail.post("/mail",postMail)
+routerMail.get("/mail",(_req:any, res:any)=>{
+    res.send("hello world!")
+})
 
 export default routerMail;
