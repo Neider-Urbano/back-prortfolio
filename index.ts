38b0f750e93c530c8d9c1e31211ce10c6639ffe8
Neider Urbano <jjuly8julianur@gmail.com>
2022-10-11 13:34:53 -0500

    files backend

---
+++ index.ts
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+const server = require("./server");
+
+server.listen(PORT,()=>{
+    console.log("App running in the port: "+PORT)
+})
---
+++ server.ts
@@ -0,0 +1,35 @@
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+require("dotenv").config();
+const {url_origin} = process.env;
+import express from 'express'
+import routerMail from "./microservices/email/route"
+
+var server=express();
+server.use(express.json())
+server.use(cors({ origin: url_origin }));
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Expose-Headers", "Authorization")
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+server.use("/",routerMail)
+
+server.use((err:any, res:any) => { 
+  const status = err.status || 500;
+  const message = err.message || err;
+  res.status(status).send(message);
+});
+
+module.exports = server;
